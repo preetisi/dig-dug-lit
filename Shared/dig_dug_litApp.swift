b04fe44649354f848699fb7cbd878e1f7da276d2
Preeti <singhpreeti236@gmail.com>
2021-06-07 19:02:07 -0400

    First commit

---
+++ Shared/dig_dug_litApp.swift
@@ -0,0 +1,17 @@
+//
+//  dig_dug_litApp.swift
+//  Shared
+//
+//  Created by Preeti Singh on 6/7/21.
+//
+
+import SwiftUI
+
+@main
+struct dig_dug_litApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
