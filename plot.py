0f7e8d49fd902c7ef59b46a1095e3dab505228d2
Preeti <singhpreeti236@gmail.com>
2021-06-08 14:36:40 -0400

    get the data using bioindex

--- plot.py
+++ plot.py
@@ -1,79 +1,30 @@
-import altair as alt
-import math
+import streamlit as st
+import pandas as pd
+import functools
+import numpy as np
+import requests
+import graphql.utilities as graphql
 
 
-try:
-    # use the altair_viewer module if it exists and we're in a notebook
-    from IPython import get_ipython
-
-    if get_ipython().__class__.__name__ == 'ZMQInteractiveShell':
-        import altair_viewer
-
-        # enable it for embedding inline
-        alt.renderers.enable('altair_viewer', inline=True)
-except ModuleNotFoundError:
-    pass
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
 
-def genes(df, name='name', start='start', end='end'):
-    """
-    Line plot of genes along an x-axis.
-    """
-    pass
+
+def load_data():
+    data = requests.get(f'https://bioindex.hugeamp.org/api/bio/query/genes?q=slc30a8&fmt=row').json()['data']
+    # build a dataframe
+    dataframe = pd.DataFrame(data)
+    st.write("Here's our first attempt at using data to create a table:")
+    st.write(dataframe)
+    return data
 
 
-def phewas(df, variant='varId', p='pValue', trait='phenotype'):
-    """
-    Returns an Altair plot for PheWAS associations and the selection
-    source for the phenotypes.
-    """
-    df = df.sort_values(by=p, ascending=True)
-    df = df.drop_duplicates(subset=[trait])
 
-    # create a log10 column for the y-axis
-    df['-log10(p)'] = df[p].map(lambda p: -math.log10(p))
-
-    # create the plot
-    chart = alt.Chart(df).mark_bar().encode(
-        y='phenotype',
-        x='-log10(p)',
-        color=alt.Color('phenotype', legend=None),
-        tooltip=[variant, trait, p],
-    )
-
-    if not variant:
-        return chart
-
-    return chart
-
-
-def associations(df, p='pValue', pos='position', trait='phenotype', effect='beta', marker='varId'):
-    """
-    Scatter plot of associations for a set of phenotype.
-    """
-    df['-log10(p)'] = df[p].map(lambda p: -math.log10(p))
-
-    # create the plot
-    return alt.Chart(df).mark_circle(size=60).encode(
-        x=alt.X(field=pos, type='quantitative', scale=alt.Scale(zero=False, nice=False)),
-        y='-log10(p)',
-        color=trait,
-        tooltip=[i for i in [marker, trait, p, effect]],
-    )
-
-
-def enrichment(df, trait='phenotype', tissue='tissue', annotation='annotation', fold='fold'):
-    """
-    Bubble plot of tissue fold enrichments per trait.
-    """
-    df = df.sort_values(by=fold, ascending=False)
-    df = df.drop_duplicates(subset=['phenotype', 'tissue', 'annotation'])
-
-    return alt.Chart(df).mark_circle().encode(
-        x=annotation,
-        y=tissue,
-        color=trait,
-        size=fold,
-        tooltip=[tissue, trait, annotation, fold]
-    )
-
+# Create a text element and let the reader know the data is loading.
+data_load_state = st.text('Loading data...')
+# Load 10,000 rows of data into the dataframe.
+data = load_data()
+# Notify the reader that the data was successfully loaded.
+data_load_state.text('Loading data...using st cache!')
