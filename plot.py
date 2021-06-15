6f5f63a1dc55e7b02d49be18e7be6e45dbe4bfdf
Preeti <singhpreeti236@gmail.com>
2021-06-15 06:57:07 -0400

    working graphql client

--- plot.py
+++ plot.py
@@ -3,28 +3,28 @@ import pandas as pd
 import functools
 import numpy as np
 import requests
-import graphql.utilities as graphql
+import graphql.utilities as gl
 
-
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
-
-
-
-def load_data():
-    data = requests.get(f'https://bioindex.hugeamp.org/api/bio/query/genes?q=slc30a8&fmt=row').json()['data']
-    # build a dataframe
-    dataframe = pd.DataFrame(data)
-    st.write("Here's our first attempt at using data to create a table:")
-    st.write(dataframe)
-    return data
-
-
-
-# Create a text element and let the reader know the data is loading.
+def query(q, concat=False):
+    resp = requests.post(f'https://bioindex.hugeamp.org/api/bio/query', data=q)
+    if resp.status_code != 200:
+        st.write(resp)
+        raise RuntimeError(resp.json()['detail'])
+        #concatenate all the results together into a single frame
+    if concat:
+        return pd.DataFrame(itertools.chain.from_iterable(resp.json()['data'].values()))
+            #values are in insertion order of the query
+#        st.write(pd.DataFrame())
+    for k, rs in resp.json()['data'].items():
+#        st.write(pd.DataFrame(rs))
+        x = {k:pd.DataFrame(rs)}
+    
+    return {k: pd.DataFrame(rs) for k, rs in resp.json()['data'].items()}
+        
+#Create a text element and let the reader know the data is loading.
 data_load_state = st.text('Loading data...')
-# Load 10,000 rows of data into the dataframe.
-data = load_data()
+q = """{GlobalEnrichment(phenotype: "ldl") {annotation, tissue, SNPs, expectedSNPs}}"""
+ # Load 10,000 rows of data into the dataframe.
+data = query(q)
 # Notify the reader that the data was successfully loaded.
 data_load_state.text('Loading data...using st cache!')
